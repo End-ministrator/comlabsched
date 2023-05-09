1ea04e31056fab85c986526fe07c00876377b339
M-sndjn <122774556+M-sndjn@users.noreply.github.com>
2023-05-09 10:20:44 +0800

    outputcss lng

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -4,11 +4,11 @@
 
 @section('content')
 <div class="flex flex-row items-center justify-center mx-24 my-24 ">
-    <div class="w-60 h-112 basis-1/2 flex border shadow-black shadow-md bg-[url('/images/bgg.jpg')] bg-no-repeat bg-cover rounded-md relative inset-x-12 ">
+    <div class="w-60 h-128 basis-1/2 flex  shadow-black shadow-md bg-[url('/images/bgg.jpg')] bg-no-repeat bg-cover rounded-md relative inset-x-12 ">
 
     </div>
-    <div class="w-60 h-128 basis-1/2 flex border shadow-black shadow-md bg-scarlet rounded-md relative flex-col items-center justify-evenly  backdrop-filter backdrop-blur-sm bg-opacity-40 ">
-        <div>
+    <div class="w-60 h-120 basis-1/3 flex  shadow-black shadow-md bg-scarlet rounded-md relative flex-col items-center justify-evenly  backdrop-filter backdrop-blur-sm bg-opacity-40 ">
+        <div class="ml-4">
             
             <div class="flex  justify-center items-center mb-10 ">
                 <!-- logo --><i class="fa-solid fa-user-secret text-5xl"></i>
@@ -19,7 +19,7 @@
                 <span class="text-md">Good day! please enter your details </span>
             </div>
         </div>
-            <form action="/login" method="post" class="flex flex-col w-96 ">
+            <form action="/login" method="post" class="flex flex-col w-80 ">
                 @csrf
                 <label for="email" class=" text-black  block my-2 text-sm">Email</label>
                 <input type="email" name="email" id="email" required class="w-full flex rounded-sm  focus:outline-none shadow-inner focus:outline-4  ">
