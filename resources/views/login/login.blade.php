04ab505e2e9608f4f13105a282a57c7c6ac74c78
M-sndjn <122774556+M-sndjn@users.noreply.github.com>
2023-05-09 08:18:06 +0800

    update

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -4,10 +4,10 @@
 
 @section('content')
 <div class="flex flex-row items-center justify-center mx-24 my-24 ">
-    <div class="w-60 h-112 basis-1/2 flex bg-[url('/images/bgg.jpg')] bg-no-repeat bg-cover rounded-md relative inset-x-12 ">
+    <div class="w-60 h-112 basis-1/2 flex border shadow-black shadow-md bg-[url('/images/bgg.jpg')] bg-no-repeat bg-cover rounded-md relative inset-x-12 ">
 
     </div>
-    <div class="w-60 h-128 basis-1/2 flex  bg-amber-500 rounded-md relative flex-col items-center justify-evenly  backdrop-filter backdrop-blur-sm bg-opacity-40 ">
+    <div class="w-60 h-128 basis-1/2 flex border shadow-black shadow-md bg-scarlet rounded-md relative flex-col items-center justify-evenly  backdrop-filter backdrop-blur-sm bg-opacity-40 ">
         <div>
             
             <div class="flex  justify-center items-center mb-10 ">
@@ -21,15 +21,15 @@
         </div>
             <form action="/login" method="post" class="flex flex-col w-96 ">
                 @csrf
-                <label for="email" class=" text-gray-500 block my-2 text-sm">Email</label>
-                <input type="email" name="email" id="email" required class="w-full flex rounded-sm">
+                <label for="email" class=" text-black  block my-2 text-sm">Email</label>
+                <input type="email" name="email" id="email" required class="w-full flex rounded-sm  focus:outline-none shadow-inner focus:outline-4  ">
             
                 <br>
-                <label for="password" class=" text-gray-500 block my-2 text-sm" >Password</label>
-                <input type="password" name="password" id="password" required class="w-full px-2 flex rounded-sm">
+                <label for="password" class=" text-black  block my-2 text-sm" >Password</label>
+                <input type="password" name="password" id="password" required class="w-full px-2 flex rounded-sm focus:outline-none shadow-inner focus:outline-4   ">
                 <br>
                 <div class=" container items-center my-5">
-                <input type="submit" value="Login" class="  bg-gray-500  hover:bg-gray-800 text-sm flex w-full justify-center px-2 rounded-md border border-transparent ">
+                <input type="submit" value="Login" class="  bg-red-400 hover:bg-red-200 text-sm flex w-full justify-center px-2 rounded-md border border-transparent ">
                 </div>
                 @error('email') 
                     <p class="error ">{{ $message }}</p> 
