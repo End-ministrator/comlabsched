298e58d0530d4ef201cc55eb25001ebac655ed40
end-ministrator <tarrasismael@gmail.com>
2023-07-03 02:13:13 +0800

    .

--- app/access_control/access_control.py
+++ app/access_control/access_control.py
@@ -1,3 +1,6 @@
+import sys
+import os
+import subprocess
 from time import sleep
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
@@ -14,20 +17,37 @@ ser = serial.Serial('/dev/ttyUSB0', 9600)  # Update the port name if necessary
 reader = SimpleMFRC522()
 tag_id = None
 
+def restart():
+    python = sys.executable
+    os.execl(python, python, * sys.argv)
+
+# def changeport():
+#     ser = serial.Serial('/dev/ttyUSB1', 9600)  # Update the port name if necessary
+
+def runTest():
+    subprocess.run(["python", "test.py"])
+
 # Set the URL of the Laravel route
-url = 'http://127.0.0.1:8000/start-rfid-scanning'
+url = 'labsched.local/start-rfid-scanning'
 headers = {'Content-Type': 'application/json',}
 
-while tag_id is None:
-    tag_id = reader.read_id()
-    data = {'tag_id': tag_id}
-    response = requests.post(url, headers=headers, json=data)
-    if response.status_code == 200:
-        print('Request sent successfully!')
-        data = "Granted"
-    else:
-        print('Request failed with status code:', response.status_code)
-        data = "Denied"
-    ser.write(data.encode())
-    sleep(10)
-    tag_id = None
+try:
+    while tag_id is None:
+        tag_id = reader.read_id()
+        data = {'tag_id': tag_id}
+        response = requests.post(url, headers=headers, json=data)
+        if response.status_code == 200:
+            print('Request sent successfully!')
+            data = "Granted"
+        else:
+            print('Request failed with status code:', response.status_code)
+            data = "Denied"
+        ser.write(data.encode())
+        sleep(10)
+        tag_id = None
+
+except:
+    try:
+        runTest()
+    except:
+        restart()
--- app/access_control/test.py
+++ app/access_control/test.py
@@ -1,3 +1,6 @@
+import sys
+import os
+import subprocess
 from time import sleep
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
@@ -9,27 +12,43 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 # Configure the serial port
-ser = serial.Serial('/dev/ttyUSB0', 4444)  # Update the port name if necessary
+ser = serial.Serial('/dev/ttyUSB1', 9600)  # Update the port name if necessary
 
 reader = SimpleMFRC522()
 tag_id = None
 
-# Set the URL of the Laravel route
-url = 'http://127.0.0.1:8000/start-rfid-scanning'
-headers = {'Content-Type': 'application/json',}
+def restart():
+    python = sys.executable
+    os.execl(python, python, * sys.argv)
 
-while tag_id is None:
-    tag_id = reader.read_id()
-    data = {'tag_id': tag_id}
-    response = requests.post(url, headers=headers, json=data)
-    if response.status_code == 200:
-        print('Request sent successfully!')
-        data = "Granted"
-    else:
-        print('Request failed with status code:', response.status_code)
-        data = "Denied"
-    sleep(5)
-    tag_id = None
+# def changeport():
+#     ser = serial.Serial('/dev/ttyUSB1', 9600)  # Update the port name if necessary
+
+def runAccessControl():
+    subprocess.run(["python", "access_control.py"])
+
+# Set the URL of the Laravel route
+url = 'labsched.local/start-rfid-scanning'
+headers = {'Content-Type': 'application/json',}
+try:
+    while tag_id is None:
+        tag_id = reader.read_id()
+        data = {'tag_id': tag_id}
+        response = requests.post(url, headers=headers, json=data)
+        if response.status_code == 200:
+            print('Request sent successfully!')
+            data = "Granted"
+        else:
+            print('Request failed with status code:', response.status_code)
+            data = "Denied"
+        sleep(5)
+        tag_id = None
+except:
+    try:
+        runAccessControl()
+    except:
+        restart()
+    
 # Send data over the serial port
-data = "Hello Arduino!"  # Replace with your desired message
-ser.write(data.encode())
+# data = "Hello Arduino!"  # Replace with your desired message
+# ser.write(data.encode())
